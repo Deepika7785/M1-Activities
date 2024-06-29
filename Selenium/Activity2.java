a8553449601929925902730f8131ba6cfd86026c
Deepika7785 <deepikarokkam55@gmail.com>
2024-06-29 12:16:42 +0530

    Add files via upload

--- Selenium/Activity2.java
+++ Selenium/Activity2.java
@@ -3,42 +3,30 @@ package activities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-public class Activity10 {
+public class Activity2 {
     public static void main(String[] args) {
         // Set up Firefox driver
         WebDriverManager.firefoxdriver().setup();
         // Create a new instance of the Firefox driver
         WebDriver driver = new FirefoxDriver();
-        // Create the Wait object
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         // Open the page
-        driver.get("https://www.training-support.net/selenium/dynamic-controls");
+        driver.get("https://www.training-support.net/selenium/login-form");
         // Print the title of the page
         System.out.println("Home page title: " + driver.getTitle());
 
-        // Find the checkbox
-        WebElement checkbox = driver.findElement(By.className("willDisappear"));
-        // Find the toggle button and click it
-        WebElement checkboxToggle = driver.findElement(By.id("toggleCheckbox"));
-        checkboxToggle.click();
-        // Wait for the checkbox to disappear
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("willDisappear")));
-        System.out.println("Checkbox is displayed: " + checkbox.isDisplayed());
-        // Click the button again
-        checkboxToggle.click();
-        // Wait for the element to appear
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("dynamicCheckbox")));
-        System.out.println("Checkbox is displayed: " + checkbox.isDisplayed());
-        // Click the checkbox
-        driver.findElement(By.xpath("//input[@class='willDisappear']")).click();
+        // Find the username field and enter the username
+        driver.findElement(By.id("username")).sendKeys("admin");
+        // Find the password field and enter the password
+        driver.findElement(By.id("password")).sendKeys("password");
+        // Find the login button and click it
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
+
+        // Print the confirmation message
+        String message = driver.findElement(By.id("action-confirmation")).getText();
+        System.out.println("Login message: " + message);
 
         // Close the browser
         driver.close();
